ab9711488625bc6b8375958603f0e7ab6b169d4c
Mitsuo Koikawa <mkoikawa@tonoi.co.jp>
2023-09-26 12:21:39 +0900

    Create fetch_and_commit_xml.py

---
+++ .github/scripts/fetch_and_commit_xml.py
@@ -0,0 +1,23 @@
+import requests
+from datetime import datetime
+
+def fetch_xml():
+    url = 'https://www.data.jma.go.jp/developer/xml/feed/regular.xml'
+    
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.text
+
+def main():
+    xml_content = fetch_xml()
+    
+    # 現在の時間をもとにファイル名を作成
+    now = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    file_name = f"xmls/{now}.xml"
+    
+    # ファイルにXMLを保存
+    with open(file_name, 'w') as f:
+        f.write(xml_content)
+
+if __name__ == '__main__':
+    main()
