5560426df1826b421aaad65cefeadf22cf329885
Mitsuo Koikawa <mkoikawa@tonoi.co.jp>
2023-09-28 23:06:35 +0900

    fetch.pyをリファクタリングしてmain.pyに変更

--- docs/index.md
+++ docs/index.md
@@ -1,8 +1,5 @@
-# Open Data 整形済み最新データ
-[テスト](data/20230928-22:07:51-regular.xml) \([source](https://www.data.jma.go.jp/developer/xml/feed/regular.xml)\)
-
-[東京オープンデータカタログ](data/20230928-22:07:54-TokyoOpenDataCatalog) \([source](https://catalog.data.metro.tokyo.lg.jp/api/3/action/package_search?q=*:*&rows=999999&start=0)\)
-
-[東京都認証保育所一覧](data/20230928-22:07:57-ninshouichiran050801.xlsx) \([source](https://www.fukushi.metro.tokyo.lg.jp/kodomo/hoiku/ninsyo/ichiran.files/ninshouichiran050801.xlsx)\)
-
-last update: 20230928-22:07:57
+# Open Data 整形済み最新データ  
+[テスト](data/20230928-23:04:37-regular.xml) \([source](https://www.data.jma.go.jp/developer/xml/feed/regular.xml)\)  
+[東京オープンデータカタログ](data/20230928-23:04:40-TokyoOpenDataCatalog) \([source](https://catalog.data.metro.tokyo.lg.jp/api/3/action/package_search?q=*:*&rows=999999&start=0)\)  
+[東京都認証保育所一覧](data/20230928-23:04:42-ninshouichiran050801.xlsx) \([source](https://www.fukushi.metro.tokyo.lg.jp/kodomo/hoiku/ninsyo/ichiran.files/ninshouichiran050801.xlsx)\)  
+last update: 20230928-23:04:42  
---
+++ main.py
@@ -0,0 +1,50 @@
+import re, os, requests, yaml
+from datetime import datetime
+
+GHPAGE_FOLDER = 'docs'
+DATA_FOLDER = 'data'
+
+def fetch_data(url):  
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.text
+
+def create_filename(name, url):
+    # 現在の時間をもとにファイル名を作成
+    file_name = f'{ datetime.now().strftime("%Y%m%d-%H:%M:%S") }-'
+
+    # Curl形式の様々な引き数のついたURLに対応したファイル名抽出
+    pattern = r'/([^/]+?\.[a-zA-Z0-9]{1,8})$'
+    file_org = re.search(pattern, url)
+    file_name += name if file_org is None else file_org.group(1)
+
+    return file_name
+    
+def main():
+    with open('opendata.yml', 'r') as yml:
+        config = yaml.safe_load(yml)
+        od_links = config['link']
+
+    html = '# Open Data 整形済み最新データ  \n'
+
+    for od_link in od_links:
+        obj_data = fetch_data(od_link['url'])
+            
+        # ファイルに取得したDataを保存
+        file_name = create_filename(od_link['name'], od_link['url'])
+        with open(GHPAGE_FOLDER + '/' + DATA_FOLDER + '/' + file_name, 'w') as f:
+            f.write(obj_data)
+
+        ### TODO: 保存したファイルにConverter処理を行う
+        
+        # Markdown出力
+        html += '[' + od_link['title'] + '](' + DATA_FOLDER + '/' + file_name + ') \('
+        html += '[source](' + od_link['url'] + ')\)  \n'
+        
+    # GitHub Pagesで公開するトップページを作成
+    html += f'last update: { datetime.now().strftime("%Y%m%d-%H:%M:%S") }  '
+    with open(GHPAGE_FOLDER + '/index.md', 'w') as f:
+        f.write(html)
+
+if __name__ == '__main__':
+    main()
